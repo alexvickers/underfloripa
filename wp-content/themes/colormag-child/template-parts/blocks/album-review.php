ab2741a6c44ab1fb6e16bdda7474bced5e5b61e3
Alexandre Aimbiré <alexvickers@gmail.com>
2025-03-29 16:36:20 -0300

    Changed review block to bottom of page

--- wp-content/themes/colormag-child/template-parts/blocks/album-review.php
+++ wp-content/themes/colormag-child/template-parts/blocks/album-review.php
@@ -13,60 +13,6 @@ if (!$album_name || !$artist_name) {
 $formatted_mark = ($mark == 10) ? '10' : number_format((float) $mark, 1);
 ?>
 
-<style>
-    .album-review-block {
-        display: flex;
-        margin-top: 22px;
-        border-bottom: 1px solid rgba(0, 0, 0, 0.24);
-        padding-bottom: 12px;
-        margin-bottom: 22px;
-    }
-    @media screen and (max-width: 768px) {
-        .album-review-block {
-            display: block;
-            text-align: center;
-
-        }
-    }
-
-    .album-review-block .mark {
-        margin-right: 24px;
-    }
-
-    @media screen and (max-width: 768px) {
-        .album-review-block .mark {
-            margin-bottom: 12px;
-        }
-    }
-
-    .album-review-block .mark span {
-        display: inline-flex;
-        font-weight: bold;
-        font-size: 32px;
-        line-height: 32px;
-        border: 5px solid #000000;
-        border-radius: 50%;
-        padding: 10px;
-        text-align: center;
-        width: 44px;
-        height: 44px;
-        align-items: center;
-        justify-content: center;
-        letter-spacing: -2px;
-    }
-
-    .album-review-block p {
-        margin-bottom: 4px;
-    }
-
-    .album-review-block h3 {
-        margin-bottom: 6px;
-        font-size: 22px;
-        font-weight: bold;
-        text-transform: uppercase;
-    }
-</style>
-
 <div class="album-review-block">
     <div class="mark"><span><?php echo $formatted_mark; ?></span></div>
     <div>
