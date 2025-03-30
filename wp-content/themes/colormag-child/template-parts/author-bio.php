305f128ba3f294ee2ba0c617d0860a78806dfc27
Alexandre Aimbiré <alexvickers@gmail.com>
2025-03-30 16:51:25 -0300

    Replaced ACF bio with standard WP bio

--- wp-content/themes/colormag-child/template-parts/author-bio.php
+++ wp-content/themes/colormag-child/template-parts/author-bio.php
@@ -1,7 +1,7 @@
 <?php
 $author_id = get_the_author_meta('ID');
 $author_name = get_the_author();
-$author_description = get_field('author_custom_bio', 'user_' . $author_id);
+$author_bio = get_the_author_meta('description');
 $custom_avatar = get_field('custom_author_avatar', 'user_' . $author_id);
 $default_avatar = get_avatar($author_id, 100); // Gravatar fallback
 
@@ -11,13 +11,13 @@ $author_avatar = $avatar_url ? '<img src="' . esc_url($avatar_url) . '" alt="' .
 
 $author_posts_url = get_author_posts_url($author_id);
 
-if ($author_description) : ?>
+if (!empty($author_bio)) : ?>
     <div class="author-bio">
         <div class="author-avatar"><?php echo $author_avatar; ?></div>
         <div class="author-info">
             <h3>Mais sobre <?php echo esc_html($author_name); ?></h3>
-            <?php if ($author_description) : ?>
-                <p><?php echo esc_html($author_description); ?></p>
+            <?php if ($author_bio) : ?>
+                <p><?php echo esc_html($author_bio); ?></p>
             <?php else : ?>
                 <p>No bio available.</p>
             <?php endif; ?>
