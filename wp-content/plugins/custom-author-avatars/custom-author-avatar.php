d9c8ef781c75b8983a36f62ae9f0598b30537f8e
Alexandre Aimbiré <alexvickers@gmail.com>
2025-04-05 19:41:59 -0300

    Removed comments

--- wp-content/plugins/custom-author-avatars/custom-author-avatar.php
+++ wp-content/plugins/custom-author-avatars/custom-author-avatar.php
@@ -2,26 +2,22 @@
 /*
 Plugin Name: Custom Author Avatars for Magazine Blocks
 Description: Overrides author avatars in Magazine Blocks with ACF image field.
-Author: Alexandre
+Author: Alexandre Aimbiré
 Version: 1.1
 */
 
-// Replace author avatars in Magazine Blocks with ACF custom_author_avatar
 add_filter('render_block', 'replace_author_avatar_in_magazine_blocks', 10, 2);
 
 function replace_author_avatar_in_magazine_blocks($block_content, $block) {
-    // Only affect Magazine Blocks
     if (!isset($block['blockName']) || strpos($block['blockName'], 'magazine-blocks/') !== 0) {
         return $block_content;
     }
 
-    // Get the current post ID
     $post_id = get_the_ID();
     if (!$post_id) {
         return $block_content;
     }
 
-    // Get author ID and ACF avatar URL
     $author_id = get_post_field('post_author', $post_id);
     if (!$author_id) {
         return $block_content;
@@ -32,7 +28,6 @@ function replace_author_avatar_in_magazine_blocks($block_content, $block) {
         return $block_content;
     }
 
-    // Replace the Gravatar URL with ACF avatar
     $updated_content = preg_replace(
         '#https://secure\.gravatar\.com/avatar/[^"\']+#',
         esc_url($acf_avatar_url),
