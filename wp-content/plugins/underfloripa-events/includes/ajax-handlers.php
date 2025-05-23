a936edca1a7cfb44d3f64fb185d3791356d14c0e
Alexandre Aimbiré <alexvickers@gmail.com>
2025-05-23 01:32:09 -0300

    Fixed bugs on the events widget select

--- wp-content/plugins/underfloripa-events/includes/ajax-handlers.php
+++ wp-content/plugins/underfloripa-events/includes/ajax-handlers.php
@@ -7,21 +7,45 @@ if (!defined('ABSPATH')) {
 add_action('wp_ajax_uf_filter_events_by_city', 'uf_filter_events_by_city');
 add_action('wp_ajax_nopriv_uf_filter_events_by_city', 'uf_filter_events_by_city');
 
-function uf_filter_events_by_city()
-{
+function uf_filter_events_by_city() {
 	$city = sanitize_text_field($_POST['city'] ?? '');
 
 	$today = current_time('Y-m-d');
 	$two_weeks_later = date('Y-m-d', strtotime('+14 days', current_time('timestamp')));
 
-	$tax_query = [];
+	$meta_query = [
+		[
+			'key' => 'event_date',
+			'value' => [$today, $two_weeks_later],
+			'compare' => 'BETWEEN',
+			'type' => 'DATE',
+		]
+	];
 
 	if (!empty($city)) {
-		$tax_query[] = [
-			'taxonomy' => 'venue_city',
-			'field' => 'slug',
-			'terms' => $city,
-		];
+		$venues = get_posts([
+			'post_type' => 'venue',
+			'tax_query' => [
+				[
+					'taxonomy' => 'venue_city',
+					'field' => 'slug',
+					'terms' => $city,
+				]
+			],
+			'fields' => 'ids',
+			'posts_per_page' => -1,
+		]);
+
+		if ($venues) {
+			$meta_query[] = [
+				'key' => 'venue_post',
+				'value' => $venues,
+				'compare' => 'IN',
+			];
+		} else {
+			echo '<li class="event">Nenhum evento encontrado.</li>';
+			wp_die();
+		}
 	}
 
 	$query_args = [
@@ -30,26 +54,14 @@ function uf_filter_events_by_city()
 		'meta_key' => 'event_date',
 		'orderby' => 'meta_value',
 		'order' => 'ASC',
-		'meta_query' => [
-			[
-				'key' => 'event_date',
-				'value' => [$today, $two_weeks_later],
-				'compare' => 'BETWEEN',
-				'type' => 'DATE',
-			]
-		],
+		'meta_query' => $meta_query,
 	];
-
-	if (!empty($tax_query)) {
-		$query_args['tax_query'] = $tax_query;
-	}
 
 	$query = new WP_Query($query_args);
 
 	if ($query->have_posts()) {
 		while ($query->have_posts()) {
 			$query->the_post();
-			$event_id = get_the_ID();
 			$title = get_the_title();
 			$date = get_field('event_date');
 			$doors_time = get_field('doors_time');
@@ -59,9 +71,12 @@ function uf_filter_events_by_city()
 			$venue_name = $venue_city = '';
 			if ($venue) {
 				$venue_name = get_the_title($venue->ID);
-				$term = get_field('venue_city', $venue->ID);
-				if ($term && is_object($term)) {
-					$venue_city = $term->name;
+				$venue_city = '';
+				if ($venue) {
+					$terms = wp_get_post_terms($venue->ID, 'venue_city');
+					if (!is_wp_error($terms) && !empty($terms)) {
+						$venue_city = $terms[0]->name;
+					}
 				}
 			}
 
