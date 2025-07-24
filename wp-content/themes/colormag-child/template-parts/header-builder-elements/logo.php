8f7f03648c09972916345df80a23cb2e70f706e6
Alexandre Aimbiré <alexvickers@gmail.com>
2025-07-24 16:24:30 -0300

    Added classes to header logo and title

--- wp-content/themes/colormag-child/template-parts/header-builder-elements/logo.php
+++ wp-content/themes/colormag-child/template-parts/header-builder-elements/logo.php
@@ -19,17 +19,17 @@ $site_tagline_enable  = get_theme_mod('colormag_enable_site_tagline', true);
 
 ?>
 <div id="cm-site-branding" class="cm-site-branding">
-	<a href="<?php site_url() ?>">
-		<img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/img/logo.jpg'); ?>" alt="Under Floripa Logo">
+	<a href="<?php site_url() ?>" class="custom-logo-link" rel="home" aria-current="page">
+		<img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/img/logo.jpg'); ?>" class="custom-logo lazy loaded" alt="Under Floripa">
 	</a>
 </div><!-- #cm-site-branding -->
 
 <?php
 
 if ($site_identity_enable || $site_tagline_enable) { ?>
-	<div id="cm-site-info" class="">
-		<h1 class="cm-site-title cm-title-show-desktop cm-title-show-tablet cm-title-show-mobile ">
-			<a href="https://underfloripa.com.br/" title="Under Floripa" rel="home">Under Floripa</a>
+	<div id="cm-site-info">
+		<h1 class="cm-site-title cm-title-show-desktop cm-title-show-tablet cm-title-show-mobile">
+			<a href="<?php site_url() ?>" title="Under Floripa" rel="home">Under Floripa</a>
 		</h1>
 
 		<p class="cm-site-description cm-tagline-show-desktop cm-tagline-show-tablet cm-tagline-show-mobile ">
