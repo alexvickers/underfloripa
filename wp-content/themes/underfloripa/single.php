e2149b443d073bad627de785fc740dcb2477249d
Alexandre Aimbiré <alexvickers@gmail.com>
2025-08-30 00:29:53 -0300

    Added ads to single.php

--- wp-content/themes/underfloripa/single.php
+++ wp-content/themes/underfloripa/single.php
@@ -1,7 +1,7 @@
 <?php
 // Exit if accessed directly.
 if (! defined('ABSPATH')) {
-    exit;
+	exit;
 }
 
 get_header(); ?>
@@ -42,16 +42,44 @@ get_header(); ?>
 					<!-- Content -->
 					<div class="entry-content">
 						<?php the_content(); ?>
+						<div class="lazy-google-ad responsive-ad"
+							data-ad-client="ca-pub-2855642712528671"
+							data-ad-slot="1234567890">
+							<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2855642712528671"
+								crossorigin="anonymous"></script>
+							<ins class="adsbygoogle"
+								style="display:block; text-align:center;"
+								data-ad-layout="in-article"
+								data-ad-format="fluid"
+								data-ad-client="ca-pub-2855642712528671"
+								data-ad-slot="3442625715"></ins>
+							<script>
+								(adsbygoogle = window.adsbygoogle || []).push({});
+							</script>
+						</div>
 					</div>
-
 					<?php get_template_part('template-parts/author-bio', 'box'); ?>
-
 				</article>
 
 				<?php echo get_related_posts_block(get_post()); ?>
 
 		<?php endwhile;
 		endif; ?>
+
+		<div class="lazy-google-ad responsive-ad"
+			data-ad-client="ca-pub-2855642712528671"
+			data-ad-slot="1234567890">
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2855642712528671"
+				crossorigin="anonymous"></script>
+			<ins class="adsbygoogle"
+				style="display:block"
+				data-ad-format="autorelaxed"
+				data-ad-client="ca-pub-2855642712528671"
+				data-ad-slot="2638856769"></ins>
+			<script>
+				(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+		</div>
 
 	</main>
 
