d1df8b5854cfbe4185f8ac7cf884666f5824296e
Alexandre Aimbiré <alexvickers@gmail.com>
2026-01-07 23:47:12 -0300

    Fixed links in related post title

--- wp-content/themes/underfloripa/template-parts/related-posts-block.php
+++ wp-content/themes/underfloripa/template-parts/related-posts-block.php
@@ -10,7 +10,6 @@ if (!empty($filtered_posts)) : ?>
         <ul>
             <?php foreach ($filtered_posts as $post_item): ?>
                 <li>
-                    <a href="<?php echo get_permalink($post_item->ID); ?>">
                         <?php
                         if (!$is_resenhas) : ?>
                             <span class="related-post-date">
@@ -19,22 +18,25 @@ if (!empty($filtered_posts)) : ?>
                         <?php endif; ?>
 
                         <div class="related-post-thumb">
-                            <?php
-                            $thumb = get_the_post_thumbnail($post_item->ID, [300, 300]);
-                            if ($thumb) {
-                                echo $thumb;
-                            }
+                            <a href="<?php echo get_permalink($post_item->ID); ?>">
+                                <?php
+                                $thumb = get_the_post_thumbnail($post_item->ID, [300, 300]);
+                                if ($thumb) {
+                                    echo $thumb;
+                                }
 
-                            $categories = get_the_category($post_item->ID);
-                            if (!empty($categories) && $categories[0]->slug !== 'resenhas') {
-                                $cat_link = get_category_link($categories[0]->term_id);
-                                echo '<span class="related-post-category">
-                                    <a href="' . esc_url($cat_link) . '">' . esc_html($categories[0]->name) . '</a>
-                                </span>';
-                            } ?>
+                                $categories = get_the_category($post_item->ID);
+                                if (!empty($categories) && $categories[0]->slug !== 'resenhas') {
+                                    $cat_link = get_category_link($categories[0]->term_id);
+                                    echo '<span class="related-post-category">
+                                        <a href="' . esc_url($cat_link) . '">' . esc_html($categories[0]->name) . '</a>
+                                    </span>';
+                                } ?>
+                            </a>
                         </div>
-                        <span class="related-post-title"><?php echo get_the_title($post_item->ID); ?></span>
-                    </a>
+                        <a href="<?php echo get_permalink($post_item->ID); ?>" class="related-post-title">
+                            <?php echo get_the_title($post_item->ID); ?>
+                        </a>
                 </li>
             <?php endforeach; ?>
         </ul>
